368e1eb54e49169130d1233a7e270c4b219add57
DESKTOP-DQ7BBK3\rnwhd <rnwhdghl12@naver.com>
2017-04-25 19:00:15 +0900

    text의 불필요한 개행 삭제함수 생성

--- Translator/src/bun_yuchae_voca/Translator.java
+++ Translator/src/bun_yuchae_voca/Translator.java
@@ -14,7 +14,6 @@ public class Translator {
 	
 	public Translator() {
 		manager = KeyManager.getInstance();
-
 		// Javonet api 권한 획득	
 		try {
 			Javonet.activate("rnwhdghl12@naver.com", manager.requestKey(CompanyType.JAVONET),JavonetFramework.v45);
@@ -30,12 +29,22 @@ public class Translator {
 	public String act(String text,String from,String to) {
 		// SecretKey값이 유효하면 SecretKey값을 요청하지않음
 		String result=null;
+		text = modifyText(text);
 		try {
 			result = cs.invoke("TranslateMethod",manager.requestKey(CompanyType.MS_BING),text,from,to);
 		} catch (JavonetException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		return result;
+	}
+	private String modifyText(String text) {
+		String[] result=null;
+		result = text.split("\n");
+		StringBuilder a=new StringBuilder();
+		for(int i=0;i<result.length;++i)
+			a.append(result[i]);
+		return a.toString();
 	}
 }
