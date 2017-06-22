76715ff904ab71cd2b5e660891782e2de318bb28
구종회 <rnwhdghl12@naver.com>
2017-06-23 07:25:16 +0900

    back up

--- android/platforms/Voca/app/src/main/java/com/example/user/voca/setImage.java
+++ android/platforms/Voca/app/src/main/java/com/example/user/voca/setImage.java
@@ -1,23 +1,39 @@
 package com.example.user.voca;
+
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class setImage extends Activity{
 
     private static final int PICK_FROM_CAMERA = 1;
     private static final int PICK_FROM_GALLERY = 2;
     private ImageView imgview;
-
+    Bitmap image_bitmap;
+    String imgPath;
+    Uri uri;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +42,7 @@ public class setImage extends Activity{
         TextView text = (TextView) findViewById(R.id.tv);
         Button buttonCamera = (Button) findViewById(R.id.btn_take_camera);
         Button buttonGallery = (Button) findViewById(R.id.btn_select_gallery);
-
+        Button select = (Button)findViewById(R.id.btn_seleted);
 
         text.setTypeface(Typeface.createFromAsset(getAssets(), "HMFMPYUN.TTF"));
         buttonCamera.setTypeface(Typeface.createFromAsset(getAssets(), "HMFMPYUN.TTF"));
@@ -36,9 +52,8 @@ public class setImage extends Activity{
             public void onClick(View v) {
 
                 // 카메라 호출
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT,
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI.toString());
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
 
                 // 이미지 잘라내기 위한 크기
                 intent.putExtra("crop", "true");
@@ -58,47 +73,77 @@ public class setImage extends Activity{
 
         buttonGallery.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+                intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 100);
+            }
 
-                Intent intent = new Intent();
-                // Gallery 호출
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                // 잘라내기 셋팅
-                intent.putExtra("crop", "true");
-                intent.putExtra("aspectX", 0);
-                intent.putExtra("aspectY", 0);
-                intent.putExtra("outputX", 200);
-                intent.putExtra("outputY", 150);
-                try {
-                    intent.putExtra("return-data", true);
-                    startActivityForResult(Intent.createChooser(intent,
-                            "Complete action using"), PICK_FROM_GALLERY);
-                } catch (ActivityNotFoundException e) {
-                    // Do nothing for now
-                }
+        });
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File file = getFilesDir();
+
+                Log.d("path: ",imgPath);
+                String base = "A";
+
+                base = ImageToBase64.change(imgPath);
+                RequestAPI request = new RequestAPI();
+                String exe = imgPath.substring(imgPath.lastIndexOf(".")+1);
+                Log.d("Base:",base);
+                request.requestScanning("data:image/"+exe+";base64,"+base,"English");
             }
         });
     }
 
-    protected void onActivityResult(int requestCode,
-                                    int resultCode,
-                                    Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (requestCode == PICK_FROM_CAMERA) {
-            Bundle extras = data.getExtras();
-            if (extras != null) {
-                Bitmap photo = extras.getParcelable("data");
-                imgview.setImageBitmap(photo);
+        Toast.makeText(getBaseContext(), "resultCode : "+resultCode,Toast.LENGTH_SHORT).show();
+        if(requestCode == 100)
+        {
+            if(resultCode==Activity.RESULT_OK)
+            {
+                try {
+                    //Uri에서 이미지 이름을 얻어온다.
+                    String name_Str = getImageNameToUri(data.getData());
+                    uri=data.getData();
+                    //이미지 데이터를 비트맵으로 받아온다.
+                    image_bitmap    = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                    ImageView image = (ImageView)findViewById(R.id.imageView1);
 
-            }
-        }
-        if (requestCode == PICK_FROM_GALLERY) {
-            Bundle extras2 = data.getExtras();
-            if (extras2 != null) {
-                Bitmap photo = extras2.getParcelable("data");
-                imgview.setImageBitmap(photo);
+                    //배치해놓은 ImageView에 set
+                    image.setImageBitmap(image_bitmap);
+
+
+                    Toast.makeText(getBaseContext(), "name_Str : "+name_Str , Toast.LENGTH_SHORT).show();
+
+
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
             }
         }
     }
-}
+    public String getImageNameToUri(Uri data)
+    {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(data, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
 
+        cursor.moveToFirst();
+
+        imgPath = cursor.getString(column_index);
+
+        String imgName = imgPath.substring(imgPath.lastIndexOf("/")+1);
+
+        return imgName;
+    }
+}
